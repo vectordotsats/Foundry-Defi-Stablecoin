9c12f53bc9761ba02809ef055ecba3c4fc27a98b
Vectordotsats <Vector02ozo@gmail.com>
2025-03-05 03:38:46 +0100

    StableCoin commit

--- README.md
+++ README.md
@@ -6,3 +6,13 @@
 3. Collateral: Exogenous (Crypto)
    1. ETH
    2. BTC
+
+## Explanation of `address(0)` in Solidity
+
+In a Solidity contract, `address(0)` represents the zero address, which is a special address in the Ethereum network. It is often used as a sentinel value to indicate the absence of an address or to represent a null address. The zero address is 0x0000000000000000000000000000000000000000.
+
+Here are some common uses of `address(0)` in smart contracts:
+
+1. **Burning Tokens**: When tokens are burned, they are often sent to the zero address to remove them from circulation.
+2. **Initial Values**: It can be used to check if an address variable has been initialized or not.
+3. **Validation**: It can be used to validate that an address is not the zero address before performing certain operations.
--- src/DecStableCoin.sol
+++ src/DecStableCoin.sol
@@ -21,7 +21,7 @@
 // private
 // view & pure functions
 
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
 
 /**
  * @title DecStableCoin
@@ -34,6 +34,43 @@ pragma solidity 0.8.18;
  * This contract is meant to be governed by DSCEngine. This contract is a contract is an ERC20 implementation of the stablecoin system.
  */
 
-contract DecStableCoin {
+// IMPORTS
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol"; //Ownable in the sense that the Stable coin is governed by our Logic.
 
+// ERRORS
+error StableCoin_MustBeGreaterThanZero();
+error StableCoin_BalanceIsTooLow();
+error StableCoin_NotZeroAddress();
+
+contract DecStableCoin is ERC20Burnable, Ownable {
+    constructor() ERC20("DecStableCoin", "DSC") {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        //override overrides the Burn function in ERC20Burnable
+        uint256 balance = balanceOf(msg.sender);
+        if (balance <= 0) {
+            revert StableCoin_MustBeGreaterThanZero();
+        }
+        if (balance < _amount) {
+            revert StableCoin_BalanceIsTooLow();
+        }
+        super.burn(_amount);
+        //The "super" keyword accesses the parent contract's functions of only the function that is attatch to the "super" keyword.
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert StableCoin_NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert StableCoin_MustBeGreaterThanZero();
+        }
+        _mint(_to, _amount);
+
+        return true;
+    }
 }
