8765bb261ff1b032f3ddc6e2b4b2b224f23d33e6
Vector <vector02ozo@gmail.com>
2025-03-23 12:32:18 +0100

    test commit

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -5,5 +5,45 @@ import {Script} from "forge-std/Script.sol";
 import {DSCEngine} from "../src/DSCEngine.sol";
 
 contract HelperConfig is Script {
+    struct NetworkConfig {
+        address wethUsdPriceFeed;
+        address wbtcUsdPriceFeed;
+        address weth;
+        address wbtc;
+        uint256 deployerKey;
+    }
+    
+    NetworkConfig public activeNetworkConfig;
 
+    constructor () {
+        if(block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        }
+        else {
+            activeNetworkConfig = getOrCreateAnvilConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public view returns(NetworkConfig memory sepoliaEthConfig) {
+        sepoliaEthConfig = NetworkConfig({
+            wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306,
+            wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
+            wbtc: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
+            weth: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
+            deployerKey: vm.envUint("PRIVATE_KEY")
+        });
+
+        return sepoliaEthConfig;
+    }
+
+    function getOrCreateAnvilConfig() public view returns(NetworkConfig memory anvilConfig) {
+        
+        // Check to see if we set the active config network.
+        if(activeNetworkConfig.wethUsdPriceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        
+    
+    }
 }
