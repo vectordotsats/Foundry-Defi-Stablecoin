771533632526fdaa43ebdccf27004a6bbcb6b66e
Vector <vector02ozo@gmail.com>
2025-03-25 14:06:02 +0100

    Deploy commit

--- script/DeployDecStableCoin.sol
+++ script/DeployDecStableCoin.sol
@@ -5,15 +5,26 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {DecStableCoin} from "../src/DecStableCoin.sol";
 import {DSCEngine} from "../src/DSCEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployDecStableCoin is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
 
     function run() public returns (DecStableCoin, DSCEngine) {
-        vm.startBroadcast();
+        HelperConfig config = new HelperConfig();
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) = config.activeNetworkConfig();
+
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast(deployerKey);
         DecStableCoin decStableCoin = new DecStableCoin();
-        DSCEngine engine = new DSCEngine();
+        DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(decStableCoin));
+
+        decStableCoin.transferOwnership(address(engine));
         vm.stopBroadcast();
 
-        // return decStableCoin;
+        return (decStableCoin, engine);
     }
 }
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -18,6 +18,7 @@ contract HelperConfig is Script {
     uint8 private constant DECIMAL = 8;
     int256 private constant ETH_USD_PRICE = 2000e8;
     int256 private constant BTC_USD_PRICE = 1000e8;
+    uint256 private constant DEFAULT_ANVIL_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
     NetworkConfig public activeNetworkConfig;
 
     constructor () {
@@ -33,8 +34,8 @@ contract HelperConfig is Script {
         sepoliaEthConfig = NetworkConfig({
             wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306,
             wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
-            wbtc: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
-            weth: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
+            weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
+            wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
             deployerKey: vm.envUint("PRIVATE_KEY")
         });
 
@@ -62,6 +63,16 @@ contract HelperConfig is Script {
         ERC20Mock wbtcMock = new ERC20Mock();
 
         vm.stopBroadcast();
+
+        anvilConfig = NetworkConfig({
+            wethUsdPriceFeed: address(ethUsdPriceFeed),
+            wbtcUsdPriceFeed: address(btcUsdPriceFeed),
+            weth: address(wethMock),
+            wbtc: address(wbtcMock),
+            deployerKey: DEFAULT_ANVIL_PRIVATE_KEY
+        });
+
+        return anvilConfig;
     
     }
 }
